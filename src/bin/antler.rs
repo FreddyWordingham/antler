2bf8e001d283998ae74273c1fe3d8c12d2636257
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-09-14 01:43:50 +0100

    Cleaned initialisation function.

--- src/bin/antler.rs
+++ src/bin/antler.rs
@@ -3,7 +3,7 @@
 use arctk::{
     args,
     err::Error,
-    util::{banner, dir, exec, report},
+    util::{banner, dir, exec},
 };
 use arctk_attr::input;
 use palette::{Gradient, LinSrgba};
@@ -14,28 +14,25 @@ use std::{
 
 fn main() {
     banner::title("RENDER").expect("Failed to print title.");
-    let (params_path, in_dir, out_dir) = init().expect("Failed to initialise.");
+    let (params_path, in_dir, out_dir) = init();
 }
 
 /// Initialise the command line arguments and directories.
-fn init() -> Result<(PathBuf, PathBuf, PathBuf), Error> {
+fn init() -> (PathBuf, PathBuf, PathBuf) {
     banner::section("Initialisation").expect("Failed to print title.");
     banner::sub_section("Command line arguments").expect("Failed to print title.");
     args!(bin_path: PathBuf;
         params_path: PathBuf
     );
-    // report!("binary path", bin_path.display());
-    // report!("parameters path", params_path.display());
+    println!("{:>32} : {}", "binary path", bin_path.display());
+    println!("{:>32} : {}", "parameters path", params_path.display());
 
     banner::sub_section("Directories").expect("Failed to print title.");
-    let cwd = current_dir()?;
-    let exec_name = exec::name()?;
-    let (in_dir, out_dir) = dir::io_dirs(
-        Some(cwd.join("input").join(exec_name.clone())),
-        Some(cwd.join("output").join(exec_name)),
-    )?;
-    // report!("input directory", in_dir.display());
-    // report!("output directory", out_dir.display());
+    let cwd = current_dir().expect("Failed to determine current working directory.");
+    let (in_dir, out_dir) = dir::io_dirs(Some(cwd.join("input")), Some(cwd.join("output")))
+        .expect("Failed to initialise directories.");
+    println!("{:>32} : {}", "input directory", in_dir.display());
+    println!("{:>32} : {}", "output directory", out_dir.display());
 
-    Ok((params_path, in_dir, out_dir))
+    (params_path, in_dir, out_dir)
 }
