53034c58b4cc24ae2d767296ae95cd3460cf2014
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-09-14 01:19:19 +0100

    Added antler binary.

---
+++ src/lib.rs
@@ -0,0 +1,34 @@
+//! Library core.
+
+#![warn(
+    clippy::all,
+    clippy::cargo,
+    clippy::missing_docs_in_private_items,
+    clippy::nursery,
+    clippy::pedantic,
+    clippy::restriction
+)]
+#![allow(
+    clippy::as_conversions,
+    clippy::cast_possible_truncation,
+    clippy::cast_possible_wrap,
+    clippy::cast_precision_loss,
+    clippy::cast_sign_loss,
+    clippy::else_if_without_else,
+    clippy::float_arithmetic,
+    clippy::implicit_return,
+    clippy::indexing_slicing,
+    clippy::integer_arithmetic,
+    clippy::integer_division,
+    clippy::modulo_arithmetic,
+    clippy::panic,
+    clippy::print_stdout,
+    clippy::unreachable,
+    clippy::unwrap_used
+)]
+
+// pub mod error;
+
+// pub use self::{
+//     error::*
+// };
