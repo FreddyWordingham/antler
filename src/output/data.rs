0b80b5d4a4dd73223d714cbfcf0ddae6efe094c5
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-11-12 02:08:11 +0000

    Added output image normalisation.

--- src/output/data.rs
+++ src/output/data.rs
@@ -4,9 +4,11 @@ use arctk::{
     err::Error,
     file::Save,
     img::Image,
+    math::Vec3,
     ord::{X, Y},
 };
 use ndarray::Array2;
+use ndarray_stats::QuantileExt;
 use palette::{Gradient, LinSrgba};
 use std::{ops::AddAssign, path::Path};
 
@@ -18,6 +20,8 @@ pub struct Data {
     pub time: Array2<f64>,
     /// Distance data.
     pub dist: Array2<f64>,
+    /// Ending direction.
+    pub end_dir: Array2<Vec3>,
 }
 
 impl Data {
@@ -29,9 +33,10 @@ impl Data {
         debug_assert!(res[Y] > 0);
 
         Self {
-            img: Image::new(res, LinSrgba::new(0.0, 0.0, 0.0, 0.0)),
+            img: Image::new_blank(res, LinSrgba::new(0.0, 0.0, 0.0, 0.0)),
             time: Array2::zeros(res),
             dist: Array2::zeros(res),
+            end_dir: Array2::default(res),
         }
     }
 }
@@ -42,6 +47,7 @@ impl AddAssign<&Self> for Data {
         self.img += &rhs.img;
         self.time += &rhs.time;
         self.dist += &rhs.dist;
+        self.end_dir += &rhs.end_dir;
     }
 }
 
@@ -50,11 +56,19 @@ impl Save for Data {
     fn save(&self, out_dir: &Path) -> Result<(), Error> {
         self.img.save(&out_dir.join("render.png"))?;
 
-        let grad = Gradient::new(vec![
+        let greyscale = Gradient::new(vec![
             LinSrgba::new(0.0, 0.0, 0.0, 1.0),
             LinSrgba::new(1.0, 1.0, 1.0, 1.0),
         ]);
-        Image::data_to_linear(&self.time, &grad)?.save(&out_dir.join("time.png"))?;
-        Image::data_to_linear(&self.dist, &grad)?.save(&out_dir.join("dist.png"))
+
+        let max_time = self.time.max()?;
+        Image::new(self.time.map(|x| greyscale.get((*x / max_time) as f32)))
+            .save(&out_dir.join("time.png"))?;
+
+        let max_dist = self.dist.max()?;
+        Image::new(self.dist.map(|x| greyscale.get((*x / max_dist) as f32)))
+            .save(&out_dir.join("dist.png"))
+        // Image::data_to_linear(&self.dist, &greyscale)?.save(&out_dir.join("dist.png"))?;
+        // Image::data_to_linear(&self.end_dir, &greyscale)?.save(&out_dir.join("dist.png"))
     }
 }
