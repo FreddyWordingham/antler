0afad6d9377b6e2a56d98c6949eb92692faf8b1b
Freddy Wordingham <freddy@wordingham.ai>
2026-04-22 14:01:33 +0100

    Implemented Traceable for Sphere

--- src/geometry/sphere.rs
+++ src/geometry/sphere.rs
@@ -1,4 +1,6 @@
-use nalgebra::{Point3, Vector3};
+use std::f32::consts::{PI, TAU};
+
+use nalgebra::{Point2, Point3, Unit, Vector3};
 
 use crate::{
     geometry::{Bounded, Ray, Traceable},
@@ -20,7 +22,43 @@ impl Bounded for Sphere {
 }
 
 impl Traceable for Sphere {
-    fn trace(&self, _ray: &Ray) -> Option<Hit> {
-        todo!()
+    fn trace(&self, ray: &Ray) -> Option<Hit> {
+        let oc = ray.origin - self.centre;
+
+        let a = ray.direction.dot(&ray.direction);
+        let half_b = oc.dot(&ray.direction);
+        let c = oc.dot(&oc) - self.radius * self.radius;
+
+        let discriminant = half_b * half_b - a * c;
+        if discriminant < 0.0 {
+            return None;
+        }
+
+        let sqrt_d = discriminant.sqrt();
+
+        let mut distance = (-half_b - sqrt_d) / a;
+        if distance <= 0.0 {
+            distance = (-half_b + sqrt_d) / a;
+
+            if distance <= 0.0 {
+                return None;
+            }
+        }
+
+        let position = ray.origin + *ray.direction * distance;
+        let outward_normal = Unit::new_normalize(position - self.centre);
+
+        let local = position - self.centre;
+        let theta = (-local.y / self.radius).acos();
+        let phi = local.z.atan2(local.x);
+        let u = (phi + PI) / TAU;
+        let v = theta / PI;
+
+        Some(Hit {
+            distance,
+            position,
+            normal: outward_normal,
+            uv: Point2::new(u, v),
+        })
     }
 }
