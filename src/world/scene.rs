6cfd507a75d035aa98d506229d9e614274b47170
Freddy Wordingham <freddy@wordingham.ai>
2026-04-22 16:09:41 +0100

    Added occluded method to scene

--- src/world/scene.rs
+++ src/world/scene.rs
@@ -1,3 +1,5 @@
+use std::f32::INFINITY;
+
 use crate::{
     acceleration::Bvh,
     geometry::{Bounded, Traceable},
@@ -33,26 +35,48 @@ impl Scene {
     pub fn trace(&self, world: &World, world_ray: &WorldRay) -> Option<WorldHit> {
         let bvh = self.bvh.as_ref().expect("Must build BVH before tracing.");
 
-        let mut nearest: Option<WorldHit> = None;
+        let mut nearest = None;
+        let mut best_distance = INFINITY;
 
-        for object_id in bvh.trace(world_ray) {
+        bvh.trace_with_best(world_ray, &mut best_distance, |object_id, best_distance| {
             let object = self.get_object(object_id);
             let object_ray = world_ray.to_object_space(&object.inv_transform);
 
             let geometry = world.get_geometry(object.geometry_id);
             let Some(object_hit) = geometry.trace(&object_ray) else {
-                continue;
+                return true;
             };
 
             let world_hit = object_hit.to_world_space(&object.transform, world_ray.origin, object_id);
 
-            match &nearest {
-                Some(nearest_hit) if nearest_hit.distance <= world_hit.distance => {}
-                _ => nearest = Some(world_hit),
+            if world_hit.distance < *best_distance {
+                *best_distance = world_hit.distance;
+                nearest = Some(world_hit);
             }
-        }
+
+            true
+        });
 
         nearest
+    }
+
+    pub fn occluded(&self, world: &World, world_ray: &WorldRay, max_distance: f32) -> bool {
+        let bvh = self.bvh.as_ref().expect("Must build BVH before tracing.");
+
+        bvh.trace_any_filtered(world_ray, max_distance, |object_id| {
+            let object = self.get_object(object_id);
+            let object_ray = world_ray.to_object_space(&object.inv_transform);
+
+            let geometry = world.get_geometry(object.geometry_id);
+            match geometry.trace(&object_ray) {
+                Some(object_hit) => {
+                    let world_hit = object_hit.to_world_space(&object.transform, world_ray.origin, object_id);
+                    world_hit.distance < max_distance
+                }
+
+                None => false,
+            }
+        })
     }
 
     pub fn build(&mut self, world: &World) {
