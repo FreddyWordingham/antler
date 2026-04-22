70d1987e7e1d4fde74b60f0293095e941e51a415
Freddy Wordingham <freddy@wordingham.ai>
2026-04-22 13:02:05 +0100

    Added total_weight method to Scatter

--- src/material/scatter.rs
+++ src/material/scatter.rs
@@ -4,3 +4,9 @@ pub struct Scatter {
     pub local_weight: f32,
     pub children: Vec<Photon>,
 }
+
+impl Scatter {
+    pub fn total_weight(&self) -> f32 {
+        self.local_weight + self.children.iter().map(|child| child.weight).sum::<f32>()
+    }
+}
