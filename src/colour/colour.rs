f7ab50d4ffe272dba87091e7389affc7de1bf359
Freddy Wordingham <freddy@wordingham.ai>
2026-04-22 10:28:04 +0100

    Implemented operations for Colour. Removed alpha component from Colour. Derived common traits for Id types

--- src/colour/colour.rs
+++ src/colour/colour.rs
@@ -1,7 +1,77 @@
-use palette::Srgba;
+use std::{
+    iter::Sum,
+    ops::{Add, AddAssign, Mul, MulAssign},
+};
 
-pub struct Colour(Srgba);
+use palette::Srgb;
+
+pub struct Colour(Srgb);
 
 impl Colour {
-    pub const BLACK: Colour = Colour(Srgba::new(0.0, 0.0, 0.0, 1.0));
+    pub const BLACK: Colour = Colour(Srgb::new(0.0, 0.0, 0.0));
+
+    #[inline]
+    pub const fn new(red: f32, green: f32, blue: f32) -> Self {
+        assert!(red >= 0.0 && red <= 1.0);
+        assert!(green >= 0.0 && green <= 1.0);
+        assert!(blue >= 0.0 && blue <= 1.0);
+        Self(Srgb::new(red, green, blue))
+    }
+
+    #[inline]
+    pub fn red(&self) -> f32 {
+        self.0.red
+    }
+
+    #[inline]
+    pub fn green(&self) -> f32 {
+        self.0.green
+    }
+
+    #[inline]
+    pub fn blue(&self) -> f32 {
+        self.0.blue
+    }
+}
+
+impl Add for Colour {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self(self.0 + rhs.0)
+    }
+}
+
+impl AddAssign for Colour {
+    fn add_assign(&mut self, rhs: Self) {
+        self.0 += rhs.0;
+    }
+}
+
+impl Mul for Colour {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        Self(self.0 * rhs.0)
+    }
+}
+
+impl Mul<f32> for Colour {
+    type Output = Self;
+
+    fn mul(self, rhs: f32) -> Self::Output {
+        Self(self.0 * rhs)
+    }
+}
+
+impl MulAssign<f32> for Colour {
+    fn mul_assign(&mut self, rhs: f32) {
+        self.0 *= rhs;
+    }
+}
+
+impl Sum for Colour {
+    fn sum<I: Iterator<Item = Self>>(iter: I) -> Self {
+        iter.fold(Self::BLACK, |a, b| a + b)
+    }
 }
--- src/id/material_id.rs
+++ src/id/material_id.rs
@@ -1,1 +1,2 @@
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct MaterialId(usize);
