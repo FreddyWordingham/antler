626d8bcc067f90ea082c49c877182c7546748d49
Freddy Wordingham <freddy@wordingham.ai>
2026-04-23 18:37:20 +0100

    Added MaterialConfig

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,12 +1,14 @@
 mod defaults;
 mod geometry_config;
 mod manifest;
+mod material_config;
 mod object_config;
 mod vec2;
 mod vec3;
 
 pub use geometry_config::GeometryConfig;
 pub use manifest::Manifest;
+pub use material_config::MaterialConfig;
 pub use object_config::ObjectConfig;
 pub use vec2::Vec2;
 pub use vec3::Vec3;
