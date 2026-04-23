6e764f7c1262932f51b0a0a4efc0ead11651c5bb
Freddy Wordingham <freddy@wordingham.ai>
2026-04-23 19:45:03 +0100

    Added serde support

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,1 +1,17 @@
-fn main() {}
+use antler::prelude::*;
+
+fn main() {
+    std::fs::create_dir_all("output").unwrap();
+
+    let built = SceneConfig::load("input/scene.toml").unwrap().build().unwrap();
+
+    let image = render_image(
+        &built.world,
+        &built.scene,
+        &built.camera,
+        built.render.resolution,
+        built.render.super_samples,
+    );
+
+    image.save("output/output.png").unwrap();
+}
