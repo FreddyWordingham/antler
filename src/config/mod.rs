b168a2372d927825c93b9e1936aec7eda6b96707
Freddy Wordingham <freddy@wordingham.ai>
2026-04-23 18:49:40 +0100

    Added TransformConfig

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -4,6 +4,7 @@ mod manifest;
 mod material_config;
 mod object_config;
 mod shader_config;
+mod transform_config;
 mod vec2;
 mod vec3;
 
@@ -12,5 +13,6 @@ pub use manifest::Manifest;
 pub use material_config::MaterialConfig;
 pub use object_config::ObjectConfig;
 pub use shader_config::ShaderConfig;
+pub use transform_config::TransformConfig;
 pub use vec2::Vec2;
 pub use vec3::Vec3;
---
+++ src/config/transform_config.rs
@@ -0,0 +1,35 @@
+use nalgebra::{Similarity3, Translation3, UnitQuaternion, Vector3};
+use serde::{Deserialize, Serialize};
+
+use crate::config::{Vec3, defaults};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct TransformConfig {
+    #[serde(default)]
+    pub position: Vec3,
+
+    #[serde(default)]
+    pub rotation: Vec3,
+
+    #[serde(default = "defaults::one")]
+    pub scale: f32,
+}
+
+impl Default for TransformConfig {
+    fn default() -> Self {
+        Self {
+            position: Vec3::default(),
+            rotation: Vec3::default(),
+            scale: 1.0,
+        }
+    }
+}
+
+impl From<TransformConfig> for Similarity3<f32> {
+    fn from(value: TransformConfig) -> Self {
+        let translation = Translation3::from(Vector3::from(value.position));
+        let rotation = UnitQuaternion::from_euler_angles(value.rotation.0[0], value.rotation.0[1], value.rotation.0[2]);
+
+        Similarity3::from_parts(translation, rotation, value.scale)
+    }
+}
