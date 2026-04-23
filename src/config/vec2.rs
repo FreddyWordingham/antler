1c5e82b1b0b005d19b6f562cc29c2654d8007975
Freddy Wordingham <freddy@wordingham.ai>
2026-04-23 18:35:06 +0100

    Replaced Into derivations with From for Vec2 and Vec3

--- src/config/vec2.rs
+++ src/config/vec2.rs
@@ -4,20 +4,20 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Default, Clone, Copy, Serialize, Deserialize)]
 pub struct Vec2(pub [f32; 2]);
 
-impl Into<Point2<f32>> for Vec2 {
-    fn into(self) -> Point2<f32> {
-        Point2::new(self.0[0], self.0[1])
+impl From<Point2<f32>> for Vec2 {
+    fn from(value: Point2<f32>) -> Self {
+        Vec2([value.x, value.y])
     }
 }
 
-impl Into<Vector2<f32>> for Vec2 {
-    fn into(self) -> Vector2<f32> {
-        Vector2::new(self.0[0], self.0[1])
+impl From<Vec2> for Vector2<f32> {
+    fn from(value: Vec2) -> Self {
+        Vector2::new(value.0[0], value.0[1])
     }
 }
 
-impl Into<Unit<Vector2<f32>>> for Vec2 {
-    fn into(self) -> Unit<Vector2<f32>> {
-        Unit::new_normalize(self.into())
+impl From<Vec2> for Unit<Vector2<f32>> {
+    fn from(value: Vec2) -> Self {
+        Unit::new_normalize(value.into())
     }
 }
--- src/config/vec3.rs
+++ src/config/vec3.rs
@@ -4,20 +4,20 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Default, Clone, Copy, Serialize, Deserialize)]
 pub struct Vec3(pub [f32; 3]);
 
-impl Into<Point3<f32>> for Vec3 {
-    fn into(self) -> Point3<f32> {
-        Point3::new(self.0[0], self.0[1], self.0[2])
+impl From<Vec3> for Point3<f32> {
+    fn from(value: Vec3) -> Self {
+        Point3::new(value.0[0], value.0[1], value.0[2])
     }
 }
 
-impl Into<Vector3<f32>> for Vec3 {
-    fn into(self) -> Vector3<f32> {
-        Vector3::new(self.0[0], self.0[1], self.0[2])
+impl From<Vec3> for Vector3<f32> {
+    fn from(value: Vec3) -> Self {
+        Vector3::new(value.0[0], value.0[1], value.0[2])
     }
 }
 
-impl Into<Unit<Vector3<f32>>> for Vec3 {
-    fn into(self) -> Unit<Vector3<f32>> {
-        Unit::new_normalize(self.into())
+impl From<Vec3> for Unit<Vector3<f32>> {
+    fn from(value: Vec3) -> Self {
+        Unit::new_normalize(value.into())
     }
 }
