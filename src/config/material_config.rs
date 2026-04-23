1454a3912350bff2c49b21d614978fa736aeb5da
Freddy Wordingham <freddy@wordingham.ai>
2026-04-23 18:37:20 +0100

    Added MaterialConfig

---
+++ src/config/material_config.rs
@@ -0,0 +1,17 @@
+use serde::{Deserialize, Serialize};
+
+use crate::material::{MaterialEnum, Opaque};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type", rename_all = "snake_case")]
+pub enum MaterialConfig {
+    Opaque,
+}
+
+impl From<MaterialConfig> for MaterialEnum {
+    fn from(config: MaterialConfig) -> Self {
+        match config {
+            MaterialConfig::Opaque => MaterialEnum::Opaque(Opaque),
+        }
+    }
+}
