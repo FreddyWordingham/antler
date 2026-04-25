178a3a65b37c60ff8ea36ae2b57ab1e80a211dbd
Freddy Wordingham <freddy@wordingham.ai>
2026-04-25 23:57:28 +0100

    Added Refractive material

--- src/material/mod.rs
+++ src/material/mod.rs
@@ -2,10 +2,12 @@ mod material;
 mod mirror;
 mod opaque;
 mod reflective;
+mod refractive;
 mod scatter;
 
 pub use material::{Material, MaterialEnum};
 pub use mirror::Mirror;
 pub use opaque::Opaque;
 pub use reflective::Reflective;
+pub use refractive::Refractive;
 pub use scatter::Scatter;
