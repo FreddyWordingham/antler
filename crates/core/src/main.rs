a643243419168bf3d9b6c65b219303082d093e32
Freddy Wordingham <freddy@wordingham.ai>
2026-04-27 16:05:34 +0100

    Added simple render test

--- crates/core/src/main.rs
+++ crates/core/src/main.rs
@@ -1,15 +1,32 @@
 use antler::prelude::*;
+use nalgebra::{Point2, Point3, Vector3};
 
 fn main() {
-    let width = 64;
-    let height = 32;
-    let colour = Rgba::new(0.9, 0.1, 0.1, 1.0);
+    let width = 640;
+    let height = 320;
+    let resolution = [width, height];
 
-    let mut image = RgbaImage::filled([width, height], Rgba::TRANSPARENT);
+    let shape = Aabb::new(Point3::new(-0.5, -0.5, -0.5), Point3::new(0.5, 0.5, 0.5));
 
-    for i in 0..width.min(height) {
-        image[(i, i)] = colour;
+    let camera = Perspective::new(
+        Point3::new(4.0, -4.0, 4.0),
+        Point3::new(0.0, 0.0, 0.0),
+        Vector3::z_axis(),
+        45.0f32.to_radians(),
+    );
+    let mut image = RgbaImage::filled(resolution, Rgba::TRANSPARENT);
+
+    for y in 0..height {
+        for x in 0..width {
+            let uv = Point2::new(x as f32 / width as f32, y as f32 / height as f32);
+            let ray = camera.emit(resolution, uv);
+
+            if let Some(hit) = shape.hit(&ray) {
+                let normal = hit.normal;
+                image[(x, y)] = Rgba::new(normal.x.abs(), normal.y.abs(), normal.z.abs(), 1.0);
+            }
+        }
     }
 
-    image.save("output.png").expect("failed to save image");
+    image.save("output.png").expect("Failed to save image");
 }
