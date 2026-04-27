ad3f95f1689521b3796a4d23021b7563423605b6
Freddy Wordingham <freddy@wordingham.ai>
2026-04-27 13:39:23 +0100

    Added colour constants to Rgb

--- crates/core/src/rgb.rs
+++ crates/core/src/rgb.rs
@@ -6,6 +6,9 @@ pub struct Rgb {
 }
 
 impl Rgb {
+    pub const BLACK: Self = Self::new(0.0, 0.0, 0.0);
+    pub const WHITE: Self = Self::new(1.0, 1.0, 1.0);
+
     #[inline]
     pub const fn new(red: f32, green: f32, blue: f32) -> Self {
         assert!(0.0 <= red && red <= 1.0, "Red value must be between 0.0 and 1.0.");
