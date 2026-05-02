78bd8d2a8fe7f5c62acf14c53e99286a2cece0fa
Freddy Wordingham <freddy@wordingham.ai>
2026-05-02 19:02:39 +0100

    Added emissive support for Sphere geometry

--- crates/geometry/src/sphere.rs
+++ crates/geometry/src/sphere.rs
@@ -1,8 +1,12 @@
 use std::f32::consts::{PI, TAU};
 
 use nalgebra::{Point2, Point3, Unit, Vector3};
+use rand::{Rng, RngExt};
 
-use crate::{aabb::Aabb, bounded::Bounded, config::MIN_RAY_DISTANCE, contact::Contact, ray::Ray, traceable::Traceable};
+use crate::{
+    aabb::Aabb, bounded::Bounded, config::MIN_RAY_DISTANCE, contact::Contact, ray::Ray, sample::Sample,
+    sampleable::Sampleable, traceable::Traceable,
+};
 
 pub struct Sphere {
     centre: Point3<f32>,
@@ -66,5 +70,34 @@ impl Traceable for Sphere {
         let v = theta / PI;
 
         Some(Contact::new(distance, position, normal, Point2::new(u, v), None))
+    }
+}
+
+impl Sampleable for Sphere {
+    #[inline]
+    fn area(&self) -> f32 {
+        4.0 * std::f32::consts::PI * self.radius * self.radius
+    }
+
+    #[inline]
+    fn sample<R: Rng>(&self, rng: &mut R) -> Sample {
+        let u1 = rng.random::<f32>();
+        let u2 = rng.random::<f32>();
+
+        let z = 1.0 - 2.0 * u1;
+        let r = (1.0 - z * z).sqrt();
+        let phi = TAU * u2;
+
+        let x = r * phi.cos();
+        let y = r * phi.sin();
+
+        let normal = Unit::new_unchecked(Vector3::new(x, y, z));
+        let position = self.centre + normal.into_inner() * self.radius;
+
+        Sample {
+            position,
+            normal,
+            pdf_area: 1.0 / self.area(),
+        }
     }
 }
